58003e9f940aa3c1439cc40b3d0b2fb09d79034c
barbaradecares <barbaradecares@hotmail.com>
2018-12-12 11:09:33 -0600

    done

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,34 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  handleInputChange = event => {
+    console.log(this.state);
+    // this.setState({
+    //   [event.target.name]: event.target.value
+    // }); abstraction won't pass the tests?? weird
+
+    this.setState({
+      message: event.target.value
+    });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          id="message"
+          type="text"
+          name="message"
+          value={this.state.message}
+          onChange={this.handleInputChange}
+        />
+        <p>{this.props.maxChars - this.state.message.length} Characters left</p>
       </div>
     );
   }
