50f87930050d81d6ddeaa61ee81f474272d4bf75
github username <github email address>
2018-12-16 23:07:51 -0600

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,28 +4,72 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+      message: ""
+    };
   }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  clearTweet = () => {
+    this.setState({ tweet: "" });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (!this.state.username || !this.state.password) {
+      this.setState({ tweet: "Please use a valid username and password." });
+    } else {
+      this.props.onSubmit(this.state);
+    }
+  };
+
+  clearMessage = () => {
+    this.setState({ tweet: "" });
+  };
 
   render() {
     return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>
+              Username
+              <input
+                id="username"
+                name="username"
+                type="text"
+                value={this.state.username}
+                onChange={this.handleChange}
+                onFocus={this.clearTweet}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                onFocus={this.clearTweet}
+              />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Log in</button>
+          </div>
+        </form>
+        <p>{this.state.tweet}</p>
+      </div>
     );
   }
 }
